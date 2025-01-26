21c17594a2371ce89c068c7b660ab7e8e1bf0fab
AngerVon2468 <136974526+AngerVon2468@users.noreply.github.com>
2025-01-26 19:35:15 +1100

    Update ExampleMixin.java

--- Creating A New Mixin/ExampleMixin.java
+++ Creating A New Mixin/ExampleMixin.java
@@ -1,5 +1,10 @@
+// Mapped for Fabric's Yarn mappings (1.20.1), use https://linkie.shedaniel.dev/mappings to translate to other mappings.
 package com.example.examplemod.mixin;
 
-@Mixin()
+import net.minecraft.item.Item;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(Item.class)
 public class ExampleMixin {
 }
